44562a047931947c54baa0a23f459e17e1876c4a
zyf0706 <zyf13815628123@gmail.com>
2022-11-24 11:07:59 +0900

    modified:   Notebooks/AnalyzeTools/models.py 	modified:   Notebooks/AnalyzeTools/prepare.py 	modified:   "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/GB/Best_GB.json" 	renamed:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/LSTM/lightning_logs/version_0/events.out.tfevents.1669083513.AICON_ZZZ.12100.0" -> "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/LSTM/lightning_logs/version_0/events.out.tfevents.1669195853.AICON_ZZZ.15728.0" 	modified:   "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/LSTM/lightning_logs/version_0/hparams.yaml" 	modified:   "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/RF/Best_RF.json" 	modified:   "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/SVR/Best_SVR.json" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/00372/month/metadata.tsv" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/00372/month/tensors.tsv" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/00372/week/metadata.tsv" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/00372/week/tensors.tsv" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/checkpoints/epoch=11-step=372.ckpt" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/events.out.tfevents.1669083795.AICON_ZZZ.12100.2" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/hparams.yaml" 	deleted:    "Notebooks/Models/pork-hot/\352\262\275\353\235\275\352\260\200\352\262\251/\352\262\275\353\236\265\352\260\200\352\262\251\354\247\221\352\263\204 - \354\206\214,\353\217\274\354\247\200/TFT/lightning_logs/version_0/projector_config.pbtxt" 	new file:   Notebooks/Test.ipynb 	new file:   Notebooks/Test_PTF.ipynb 	modified:   Notebooks/main.ipynb 	modified:   TFT_prac.ipynb 	new file:   optuna_test/trial_0/epoch=0.ckpt

--- Notebooks/AnalyzeTools/models.py
+++ Notebooks/AnalyzeTools/models.py
@@ -1,8 +1,10 @@
-import torch, os
+import torch, os, optuna
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 from .prepare import read_best_params, save_best_params
 import pmdarima as pm
 
@@ -18,13 +20,11 @@ def file_saver(base_dir, product, attribute, model_name, raw):
     return f"{save_dir}/{file_name}"
 
 def params_extractor(_dict):
-    parameters = _dict.get('parameters')
     base_dir = _dict.get('base_dir')
     product = _dict.get('product')
     attribute = _dict.get('attribute')
     raw = _dict.get('raw')
-    save = _dict.get('save')
-    return parameters, base_dir, product, attribute, raw, save
+    return base_dir, product, attribute, raw
 
 def best_ml(X, y, model, params, base_dir, product, attribute, model_name, raw, save=True):
     file_path = file_saver(base_dir, product, attribute, model_name, raw)
@@ -41,6 +41,28 @@ def best_ml(X, y, model, params, base_dir, product, attribute, model_name, raw, 
 
     return model, params if file_existed else model.best_params_
 
+def HyperparamsTuning(study, obj, n_trails, patience=3, verbose=True):
+    if patience < 0:
+        raise ValueError("Patience must be positive number!")
+        
+    if verbose:
+        optuna.logging.set_verbosity(optuna.logging.INFO)
+        verbose = False
+    else:
+        optuna.logging.set_verbosity(optuna.logging.ERROR)
+
+    study.optimize(obj, n_trials=n_trails)
+
+    try:
+        study.best_params
+    except ValueError:
+        if patience != 0:
+            HyperparamsTuning(study, obj, n_trails, patience-1)
+        
+        raise ValueError("Cannot find best paramters! Please reset parameters or check data!")
+    
+    return study
+
 def autoregressive_integrated_moving_average(y_train):
     model = pm.auto_arima(y_train, error_action='ignore', suppress_warnings=True, stepwise=False, seasonal=False)
     return model
@@ -51,63 +73,271 @@ def linear_regression(X, y):
 
     return model, None
 
-def support_vector_regression(X, y, search=False, **params):
-    model = SVR()
-    best_params = None
+# Version 1
+# def support_vector_regression(X, y, search=False, **params):
+#     model = SVR()
+#     best_params = None
     
+#     if not search:
+#         model.fit(X, y)
+#         return model, best_params
+
+#     parameters, base_dir, product, attribute, raw, save = params_extractor(params)
+#     parameters = parameters if parameters else {
+#         'C': [0.1, 1, 100, 1000],
+#         'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+#         'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]
+#     }
+
+#     model, best_params = best_ml(X, y, model, parameters, base_dir, product, attribute, 'SVR', raw, save)
+#     return model, best_params
+
+# param_grid = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+
+def support_vector_regression(X, y, search=False, save=False, **params):
+    base_dir, product, attribute, raw = params_extractor(params)
+    if save and (not search):
+        raise ValueError("If save is set as True, search must be True!")
+
+    def support_vector_regression_objective(trial, X, y):
+        C = trial.suggest_categorical('C', [0.1,1, 10, 100])
+        gamma = trial.suggest_categorical('gamma', [1,0.1,0.01,0.001])
+        epsilon = trial.suggest_categorical('epsilon', [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10])
+        kernel = trial.suggest_categorical('kernel', ['rbf', 'sigmoid'])
+
+        model = SVR(
+            C=C,
+            gamma=gamma,
+            epsilon=epsilon,
+            kernel=kernel,
+        )
+
+        score = cross_val_score(model, X, y, cv=5, scoring='r2')
+        if np.isnan(score.mean()):
+            raise optuna.structs.TrialPruned()
+        return score.mean()
+    
+    best_params = None
     if not search:
+        model = SVR()
         model.fit(X, y)
         return model, best_params
+    
+    file_path = file_saver(base_dir, product, attribute, 'SVR', raw)
+    file_existed = os.path.exists(file_path)
 
-    parameters, base_dir, product, attribute, raw, save = params_extractor(params)
-    parameters = parameters if parameters else {
-        'C': [0.1, 1, 100, 1000],
-        'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
-        'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]
-    }
+    if file_existed:
+        print("--> Use the existed best parameters!")
+        best_params = read_best_params(file_path)
+        print(f"\nBest parameter for Random forest is:\n  {best_params}")
+        model = SVR(
+            C=best_params.get('C'),
+            gamma=best_params.get('gamma'),
+            epsilon=best_params.get('epsilon'),
+            kernel=best_params.get('kernel'),
+        )
+        model.fit(X, y)
+        return model, best_params
+    
+    print("--> Start searching best parameters!")
+    obj = lambda trial: support_vector_regression_objective(trial, X, y)
+    study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler())
+    study = HyperparamsTuning(study, obj, 30, verbose=False)
+    best_params = study.best_params
+    print(f"\nBest parameter for Random forest is:\n  {best_params}")
+    if save:
+        save_best_params(best_params, file_path)
 
-    model, best_params = best_ml(X, y, model, parameters, base_dir, product, attribute, 'SVR', raw, save)
+    model = SVR(
+        C=best_params.get('C'),
+        gamma=best_params.get('gamma'),
+        epsilon=best_params.get('epsilon'),
+        kernel=best_params.get('kernel'),
+    )
+    model.fit(X, y)
     return model, best_params
-    
-def random_forest(X, y, search=False, **params):
-    model = RandomForestRegressor(random_state=0)
-    best_params = None
 
+# Version 1
+# def random_forest(X, y, search=False, **params):
+#     model = RandomForestRegressor(random_state=0)
+#     best_params = None
+
+#     if not search:
+#         model.fit(X, y)
+#         return model, best_params
+    
+#     parameters, base_dir, product, attribute, raw, save = params_extractor(params)
+#     parameters = parameters if parameters else {
+#         'n_estimators': [50, 100, 150], 
+#         'max_depth': [3, 5, 7, 9],
+#         'max_features': [1, 3, 5, 7, 9],
+#         'min_samples_leaf': [1, 2, 3],
+#         'min_samples_split': [1, 2, 3],
+#     }
+
+#     model, best_params = best_ml(X, y, model, parameters, base_dir, product, attribute, 'RF', raw, save)
+#     return model, best_params 
+
+def random_forest(X, y, search=False, save=False, **params):
+    base_dir, product, attribute, raw = params_extractor(params)
+    if save and (not search):
+        raise ValueError("If save is set as True, search must be True!")
+
+    def random_forest_objective(trial, X, y):
+        n_estimators = trial.suggest_int('n_estimators', low=10, high=300, step=20)
+        max_depth = trial.suggest_int('max_depth', low=1, high=10, step=2)
+        max_features = trial.suggest_int('max_features', low=1, high=10, step=2)
+        min_samples_leaf = trial.suggest_int('min_samples_leaf', low=1, high=10, step=2)
+        min_samples_split = trial.suggest_int('min_samples_split', low=1, high=10, step=2)
+
+        model = RandomForestRegressor(
+            random_state=123,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            max_features=max_features,
+            min_samples_leaf=min_samples_leaf,
+            min_samples_split=min_samples_split,
+        )
+
+        score = cross_val_score(model, X, y, cv=5, scoring='r2')
+        if np.isnan(score.mean()):
+            raise optuna.structs.TrialPruned()
+        return score.mean()
+    
+    best_params = None
     if not search:
+        model = RandomForestRegressor(random_state=123)
         model.fit(X, y)
         return model, best_params
     
-    parameters, base_dir, product, attribute, raw, save = params_extractor(params)
-    parameters = parameters if parameters else {
-        'n_estimators': [50, 100, 150], 
-        'max_depth': [3, 5, 7, 9],
-        'max_features': [1, 3, 5, 7, 9],
-        'min_samples_leaf': [1, 2, 3],
-        'min_samples_split': [1, 2, 3],
-    }
+    file_path = file_saver(base_dir, product, attribute, 'RF', raw)
+    file_existed = os.path.exists(file_path)
 
-    model, best_params = best_ml(X, y, model, parameters, base_dir, product, attribute, 'RF', raw, save)
-    return model, best_params 
-
-def gradient_boosting(X, y, search=False, **params):
-    model = GradientBoostingRegressor(random_state=0)
-    best_params = None
-
-    if not search:
+    if file_existed:
+        print("--> Use the existed best parameters!")
+        best_params = read_best_params(file_path)
+        print(f"\nBest parameter for Random forest is:\n  {best_params}")
+        model = RandomForestRegressor(
+            random_state=123,
+            n_estimators=best_params.get('n_estimators'),
+            max_depth=best_params.get('max_depth'),
+            max_features=best_params.get('max_features'),
+            min_samples_leaf=best_params.get('min_samples_leaf'),
+            min_samples_split=best_params.get('min_samples_split'),
+        )
         model.fit(X, y)
         return model, best_params
     
-    parameters, base_dir, product, attribute, raw, save = params_extractor(params)
-    parameters = parameters if parameters else {
-        'n_estimators': [50, 100, 150], 
-        'max_depth': [3, 5, 7, 9],
-        'max_features': [1, 3, 5, 7, 9],
-        'min_samples_leaf': [1, 2, 3],
-        'min_samples_split': [1, 2, 3],
-    }
+    print("--> Start searching best parameters!")
+    obj = lambda trial: random_forest_objective(trial, X, y)
+    study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler())
+    study = HyperparamsTuning(study, obj, 30, verbose=False)
+    best_params = study.best_params
+    print(f"\nBest parameter for Random forest is:\n  {best_params}")
+    if save:
+        save_best_params(best_params, file_path)
 
-    model, best_params = best_ml(X, y, model, parameters, base_dir, product, attribute, 'GB', raw, save)
-    return model, best_params 
+    model = RandomForestRegressor(
+        random_state=123,
+        n_estimators=best_params.get('n_estimators'),
+        max_depth=best_params.get('max_depth'),
+        max_features=best_params.get('max_features'),
+        min_samples_leaf=best_params.get('min_samples_leaf'),
+        min_samples_split=best_params.get('min_samples_split'),
+    )
+    model.fit(X, y)
+    return model, best_params
+
+# Version 1
+# def gradient_boosting(X, y, search=False, **params):
+#     model = GradientBoostingRegressor(random_state=0)
+#     best_params = None
+
+#     if not search:
+#         model.fit(X, y)
+#         return model, best_params
+    
+#     parameters, base_dir, product, attribute, raw, save = params_extractor(params)
+#     parameters = parameters if parameters else {
+#         'n_estimators': [50, 100, 150], 
+#         'max_depth': [3, 5, 7, 9],
+#         'max_features': [1, 3, 5, 7, 9],
+#         'min_samples_leaf': [1, 2, 3],
+#         'min_samples_split': [1, 2, 3],
+#     }
+
+#     model, best_params = best_ml(X, y, model, parameters, base_dir, product, attribute, 'GB', raw, save)
+#     return model, best_params 
+
+def gradient_boosting(X, y, search=False, save=False, **params):
+    base_dir, product, attribute, raw = params_extractor(params)
+    if save and (not search):
+        raise ValueError("If save is set as True, search must be True!")
+
+    def gradient_boosting_objective(trial, X, y):
+        n_estimators = trial.suggest_int('n_estimators', low=10, high=300, step=20)
+        max_depth = trial.suggest_int('max_depth', low=1, high=10, step=2)
+        max_features = trial.suggest_int('max_features', low=1, high=10, step=2)
+        min_samples_leaf = trial.suggest_int('min_samples_leaf', low=1, high=10, step=2)
+        min_samples_split = trial.suggest_int('min_samples_split', low=1, high=10, step=2)
+
+        model = GradientBoostingRegressor(
+            random_state=123,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            max_features=max_features,
+            min_samples_leaf=min_samples_leaf,
+            min_samples_split=min_samples_split,
+        )
+
+        score = cross_val_score(model, X, y, cv=5, scoring='r2')
+        if np.isnan(score.mean()):
+            raise optuna.structs.TrialPruned()
+        return score.mean()
+    
+    best_params = None
+    if not search:
+        model = GradientBoostingRegressor(random_state=123)
+        model.fit(X, y)
+        return model, best_params
+    
+    file_path = file_saver(base_dir, product, attribute, 'GB', raw)
+    file_existed = os.path.exists(file_path)
+
+    if file_existed:
+        print("--> Use the existed best parameters!")
+        best_params = read_best_params(file_path)
+        print(f"\nBest parameter for Random forest is:\n  {best_params}")
+        model = GradientBoostingRegressor(
+            random_state=123,
+            n_estimators=best_params.get('n_estimators'),
+            max_depth=best_params.get('max_depth'),
+            max_features=best_params.get('max_features'),
+            min_samples_leaf=best_params.get('min_samples_leaf'),
+            min_samples_split=best_params.get('min_samples_split'),
+        )
+        model.fit(X, y)
+        return model, best_params
+    
+    print("--> Start searching best parameters!")
+    obj = lambda trial: gradient_boosting_objective(trial, X, y)
+    study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler())
+    study = HyperparamsTuning(study, obj, 30, verbose=False)
+    best_params = study.best_params
+    print(f"\nBest parameter for Random forest is:\n  {best_params}")
+    if save:
+        save_best_params(best_params, file_path)
+
+    model = GradientBoostingRegressor(
+        random_state=123,
+        n_estimators=best_params.get('n_estimators'),
+        max_depth=best_params.get('max_depth'),
+        max_features=best_params.get('max_features'),
+        min_samples_leaf=best_params.get('min_samples_leaf'),
+        min_samples_split=best_params.get('min_samples_split'),
+    )
+    model.fit(X, y)
+    return model, best_params
 
 def lstm(X, y, **params):
     hidden_sz = params.get("hidden_sz") if params.get("hidden_sz") else 64
--- Notebooks/AnalyzeTools/prepare.py
+++ Notebooks/AnalyzeTools/prepare.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_percentage_error as mape
 from sklearn.metrics import r2_score as R2
+
 import matplotlib.pyplot as plt
 import json, os, torch
 import numpy as np
@@ -10,7 +12,8 @@ def model_eval(Y_true, Y_predict, stdout=False, vis=False, ret=False, **params):
     if not params:
         MAPE, RSQUARE=  mape(Y_true, Y_predict), R2(Y_true, Y_predict)
     else:
-        scaler = params['price_scaler']
+        scaler = params['scaler']
+        
         Y_true = scaler.inverse_transform(Y_true.reshape(-1, 1))
         Y_predict = scaler.inverse_transform(Y_predict.reshape(-1, 1))
         MAPE, RSQUARE = mape(Y_true, Y_predict), R2(Y_true, Y_predict)
@@ -68,21 +71,30 @@ def best_dl(X_train, y_train, X_test, y_test, _model, **others):
 def save_best_params(best_params, file_path):
         with open(file_path, 'w') as fp:
             json.dump(best_params, fp)
-    
-def read_best_params(file_path, model_type='NDL'):
+
+# Version 1    
+# def read_best_params(file_path, model_type='NDL'):
+#     with open(file_path) as f:
+#         model_best_params = json.load(f)
+#         if model_type == 'DL':
+#             return model_best_params
+#         for key, val in model_best_params.items():
+#             model_best_params[key] = [val]
+#         return model_best_params
+
+def read_best_params(file_path):
     with open(file_path) as f:
         model_best_params = json.load(f)
-        if model_type == 'DL':
-            return model_best_params
-        for key, val in model_best_params.items():
-            model_best_params[key] = [val]
         return model_best_params
 
-def data_split(data, input_cols=None, output=None, train_size=None, **params):
+def data_split(dataframe, input_cols, output, train_size, scaling=False, **params):
+    data = dataframe.copy()
+    input_scaler, output_scaler = StandardScaler(), StandardScaler()
+
     if params.get('Model') == 'ML':
         target = f'Future_{output}'
         data[target] = data[output].shift(-1 * params.get('Future'))
-        data = data.dropna()
+        data = data[:-1 * params.get('Future')]
 
         input_cols.append(output)
 
@@ -94,18 +106,35 @@ def data_split(data, input_cols=None, output=None, train_size=None, **params):
             shuffle=False
         )
         print(f"X_train: {X_train.shape} y_train: {y_train.shape} X_test: {X_test.shape} y_test: {y_test.shape}")
+
+        if scaling:
+            X_train, y_train = input_scaler.fit_transform(X_train), output_scaler.fit_transform(y_train.reshape(-1,1))
+            X_test, y_test = input_scaler.transform(X_test), output_scaler.transform(y_test.reshape(-1,1))
+            return X_train, X_test, y_train.ravel(), y_test.ravel(), input_scaler, output_scaler
         return X_train, X_test, y_train, y_test
+
+    if not scaling:
+        raise ValueError("When training deep learning models, scaling must be set as True!")
+
+    training_cutoff = params.get('dividing_line')
+    device = params.get('device')
+
+    data.iloc[:training_cutoff-1, input_cols] = input_scaler.fit_transform(data.iloc[:training_cutoff-1, input_cols])
+    data.iloc[:training_cutoff-1, [output]] = output_scaler.fit_transform(data.iloc[:training_cutoff-1, [output]])
 
     X, y = make_dataset(data[input_cols].values, data[output].values, params['future'], params['past'])
 
-    X_train, y_train = X[:params['dividing_line'], :, [-1]], y[:params['dividing_line'], -1]
-    X_test, y_test = X[params['dividing_line']:, :, [-1]], y[params['dividing_line']:, -1]
+    X_train, y_train = input_scaler.fit_transform(X_train), output_scaler.fit_transform(y_train)
+    X_test, y_test = input_scaler.transform(X_test), output_scaler.transform(y_test)
+
+    X_train, y_train = X[:training_cutoff, :, [-1]], y[:training_cutoff, -1]
+    X_test, y_test = X[training_cutoff:, :, [-1]], y[training_cutoff:, -1]
     print(f"X_train_dl: {X_train.shape} y_train_dl: {y_train.shape} X_test_dl: {X_test.shape} y_test_dl: {y_test.shape}")
 
-    X_train, y_train = torch.from_numpy(X_train).to(params['device']), torch.from_numpy(y_train).to(params['device'])
-    X_test, y_test= torch.from_numpy(X_test).to(params['device']), torch.from_numpy(y_test).to(params['device'])
+    X_train, y_train = torch.from_numpy(X_train).to(device), torch.from_numpy(y_train).to(device)
+    X_test, y_test= torch.from_numpy(X_test).to(device), torch.from_numpy(y_test).to(device)
 
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, input_scaler, output_scaler
 
 def make_dataset(X_arr, y_arr, future, past):
     X, y = [], []
