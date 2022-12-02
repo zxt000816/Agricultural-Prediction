1d4afc597d3db5d8d3596aa93eef75ca3dd0d4f5
zyf0706 <zyf13815628123@gmail.com>
2022-12-02 16:58:08 +0900

    modified:   Notebooks/AnalyzeTools/preprocess.py 	modified:   Notebooks/Forecasting (single-1-step).ipynb 	new file:   Notebooks/Test_IsolationForest.ipynb 	modified:   Notebooks/Test_Preprocessing.ipynb

--- Notebooks/AnalyzeTools/preprocess.py
+++ Notebooks/AnalyzeTools/preprocess.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_selection import SelectKBest, f_regression
+from math import floor
+from sklearn.ensemble import IsolationForest
+import matplotlib.pyplot as plt
 from termcolor import colored
+from dateutil.relativedelta import relativedelta
 
-def preprocessData(dataframe, time_col, target, prepared=False, fs=True, parse_date=True):
+# Currently, only single time series preprocessing is supported
+def preprocessData(dataframe, time_col, target, period='Day', prepared=False, fs=True, parse_date=True, fill_missing_dates=True):
     # prerequirments:
     # dataframe must contain time columns that formatted as XXXX-XX-XX or XXXX/XX/XX
     # dataframe must contain target columns, such as retail price
@@ -11,9 +16,7 @@ def preprocessData(dataframe, time_col, target, prepared=False, fs=True, parse_d
     # if fs (feature selection) set as False, skip step 4
 
     df = dataframe.copy()
-    df[target] = df[target].apply(lambda x: np.nan if x == 0 else x)
-    df[target] = df[target].interpolate(method='linear', limit_direction='both')
-    
+
     if not prepared:
         # step 1
         features = removeNoCorrCols(df, target)
@@ -22,19 +25,33 @@ def preprocessData(dataframe, time_col, target, prepared=False, fs=True, parse_d
         # step 2
         removeFirstNaRows(df, features)
 
-        # step 3
+        # step 3:
         fillNa(df, features)
     
-    # step 4
+    # Resolve error caused by prediction target containing 0
+    df[target] = df[target].apply(lambda x: np.nan if x == 0 else x)
+    df[target] = df[target].interpolate(method='linear', limit_direction='forward')
+
+    # step 4: select importance features
     if fs:
         features = featureSelection(df, features, target)
     
-    # step 5
+    # step 5: filter almost same columns
     if not prepared:
         features = filterSameCols(df, features, target)
     
-    df.index = range(len(df))
     print(f"\n-->Final features:\n  {features}")
+
+    # step 6: fill missing dates
+    if fill_missing_dates:
+        df[time_col] = pd.to_datetime(df[time_col])
+        
+        no_time_cols = [*features, target]
+        missing_dates = fillMissingDates(df, time_col, no_time_cols, period)
+        
+        df = pd.concat((df, missing_dates), axis=0).sort_values([time_col])
+        df[no_time_cols] = df[no_time_cols].interpolate(method='linear', limit_direction='forward')
+
     # parse date
     if parse_date:
         dateParser(df, time_col)
@@ -42,6 +59,7 @@ def preprocessData(dataframe, time_col, target, prepared=False, fs=True, parse_d
     else:
         df = df[[time_col] + features + [target]]
 
+    df.index = range(len(df))
     return df, features
 
 def removeNoCorrCols(dataframe, target):
@@ -115,3 +133,75 @@ def dateParser(dataframe, time_col):
     dataframe['month'] = dataframe[time_col].dt.month
     dataframe['week'] = dataframe[time_col].dt.isocalendar().week
     dataframe['day'] = dataframe[time_col].dt.day
+
+def fillMissingDates(dataframe, time_col, no_time_cols, period):
+    df = dataframe.copy()
+    prev_date = None
+    missing_dates = []
+    for _, row in df.iterrows():
+        if prev_date:
+            if period == 'Day':
+                date_gap = row[time_col].day - prev_date.day
+                if date_gap != 1:
+                    for d in range(1, date_gap):
+                        missing_dates.append([prev_date + relativedelta(days=d), *np.repeat([np.nan], len(no_time_cols))])
+            
+            if period == 'Week':
+                date_gap = row[time_col].week - prev_date.week
+                if date_gap != 1:
+                    for w in range(1, date_gap):
+                        missing_dates.append([prev_date + relativedelta(weeks=w), *np.repeat([np.nan], len(no_time_cols))])
+
+            if period == 'Month':
+                date_gap = row[time_col].month - prev_date.month
+                if date_gap != 1:
+                    for m in range(1, date_gap):
+                        missing_dates.append([prev_date + relativedelta(months=m), *np.repeat([np.nan], len(no_time_cols))])
+
+        prev_date = row[time_col]
+
+    missing_dates = pd.DataFrame(missing_dates, columns=[time_col, *no_time_cols])
+
+    return missing_dates
+
+def removeOutliers(dataframe, test_size, target_col, vis=True, **params):
+    data = dataframe.copy()
+
+    test_size = floor(len(data) * test_size) if type(test_size) == float else test_size
+    training_data = data[:-1*test_size]
+    training_idxs = training_data.index
+
+    n_estimators = params.get('n_estimators') if params.get('n_estimators') else 100
+    contamination = params.get('contamination') if params.get('contamination') else 0.03
+    
+    iforest = IsolationForest(n_estimators=n_estimators, contamination=contamination, max_samples='auto')
+    prediction = iforest.fit_predict(training_data[[target_col]])
+
+    print("Number of outliers detected: {}".format(prediction[prediction < 0].sum()))
+    print("Number of normal samples detected: {}".format(prediction[prediction > 0].sum()))
+
+    if vis:
+        normals = []
+        outliers = []
+        for value, label in zip(training_data[target_col].values, prediction):
+            if label not in [1, -1]:
+                print(label)
+            if label == 1:
+                normals.append(value)
+                outliers.append(None)
+            elif label == -1:
+                normals.append(None)
+                outliers.append(value)
+        plt.figure(figsize=(12,7))
+        plt.plot(normals, label='normal')
+        plt.plot(outliers, label='outlier')
+        plt.legend()
+        plt.show()
+    
+    for idx, label in zip(training_idxs, prediction):
+        if label == -1:
+            data.loc[idx, target_col] = np.nan
+
+    data[target_col] = data[target_col].interpolate(method='linear', limit_direction='both')
+
+    return data
